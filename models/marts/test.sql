b9fff4050411280ee85aa4aaf6d161a9ebe39685
Alexander Kruk <Alexander.Kruk@ey.com>
2026-06-09 19:39:38 -0400

    upgraded to dbt fusion

---
+++ models/marts/test.sql
@@ -0,0 +1,7 @@
+{{invocation_id}},
+{{run_started_at}},
+{{invocation_args_dict["invocation_command"]}},
+{{target.profile_name}},
+{{target.name}},
+{{target.user}},
+{{target.version}}
--- models/stg/stg_btc.sql
+++ models/stg/stg_btc.sql
@@ -2,11 +2,13 @@
     config(
         materialized='incremental', 
         incremental_strategy='merge',
-        unique_key='HASH_KEY'
+        unique_key='HASH_KEY',
+        static_analysis = 'strict'
     )
 }}
 
 select
+'{{invocation_id}}' as invocation_id,
 *
 from {{ source('btc', 'btc') }}
 
